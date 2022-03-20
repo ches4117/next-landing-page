d1f134d91b249b5e4221970cc4de4d57bc85f358
ches4117 <ches4117@gmail.com>
2022-03-20 15:03:32 +0800

    feat: add head title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from "next";
 import Image from "next/image";
+import Head from "next/head";
 import Intro from "./Intro";
 import "./index.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>ches4117</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Intro />
     </div>
   );
